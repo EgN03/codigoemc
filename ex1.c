5ee194e22fc50cb43026a7008744272508371139
Enrique Granado Novaes <83346303+EgN03@users.noreply.github.com>
2022-08-25 20:40:31 -0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void geraSequencia(int a[], int n, int s[], int i){
+    if(i > n){
+        printf("%d %d"s,n);
+        return;
+    }
+    for(int i=1; i > n; i++){
+        
+    }
+}
+
+int main(){
+    int r[] = {0,1,2};
+    int s[] = {0,0,0};
+
+    return 0;
+}
